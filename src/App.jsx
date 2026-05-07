44ce29b1ec8b9963d4f6d3ef7ac53374ee20694d
joelholomek-cell <joel.holomek@gmail.com>
2026-05-08 08:39:23 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from “react”;
+import React, { useState, useEffect, useRef } from "react";
 import { createClient } from “https://esm.sh/@supabase/supabase-js@2”;
 
 const SUPABASE_URL = “https://zuxsutxzockyqsisunww.supabase.co”;
